419b0f8f4039483ad3bf0199a70cd9f90e9ebdca
13asTerem <you@example.com>
2026-03-30 13:37:50 +0200

    writeToFile megirva

--- src/main/java/com/example/Solution.java
+++ src/main/java/com/example/Solution.java
@@ -10,13 +10,22 @@
 
 
 
+/*
+* File: Solution.java
+* Author: Doktor Kristóf Márk
+* Copyright: 2026, Doktor Kristóf Márk
+* Group: Szoft I/N
+* Date: 2026-03-30
+* Github: https://github.com/Dokkristof/
+* Licenc: MIT
+*/
+
 package com.example;
+
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
-import java.io.FileWriter;
-import java.io.IOException;
 
 public class Solution {
 
@@ -42,7 +51,7 @@ public class Solution {
         }
 
         return array;
-    }
+    };
 
     private void printAverage(double[] array) {
         double sum = 0;
@@ -51,33 +60,19 @@ public class Solution {
             sum += value;
         }
 
-        double average;
-
-        if (array.length > 0) {
-            average = sum / array.length;
-        } else {
-            average = 0;
-        }
+        double average = (array.length > 0) ? sum / array.length : 0;
 
         System.out.println("Átlaghőmérséklet: " + average);
     }
+
     private void writeToFile(double[] array, String fileName) {
-            try {
-                FileWriter writer = new FileWriter(fileName);
-
-                for (double value : array) {
-                    writer.write(value + " ");
-                }
-
-                writer.close();
-                System.out.println("Sikeres mentés: " + fileName);
-
-            } catch (IOException e) {
-                System.out.println("Hiba! Probléma történt a fájl írásakor!");
+        try (FileWriter writer = new FileWriter(fileName)) {
+            for (double t : array) {
+                writer.write(t + " ");
             }
+            System.out.println("Mentés sikeres: " + fileName);
+        } catch (IOException e) {
+            System.out.println("Hiba a fájl írásakor!");
+        }
     }
-
-
-
 }
-    
